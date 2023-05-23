4e0548a5da80b065406671667986c67bd78cea89
yaqoob75 <s2019266075@gmail.com>
2023-05-23 11:01:15 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap5/src/css/bootstrap.min.css";
+import "../node_modules/bootstrap5/src/js/bootstrap.bundle.min.js";
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
+// import Footer from "./Footer";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+    return (
+        <>
+        <Navbar/>
+        <Routes>
+            <Route exact path="/" Component={Home}/>
+            <Route exact path="/about" Component={About}/>
+            <Route exact path="/service" Component={Service}/>
+            <Route exact path="/contact" Component={Contact}/>
+        </Routes>
+
+        {/* <Footer/> */}
+        </>
+    )
 }
 
 export default App;
